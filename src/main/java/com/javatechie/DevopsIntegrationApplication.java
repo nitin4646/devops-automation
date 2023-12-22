76aeb08b948c56e14bbbe7e69abbd3bf513cc953
nitin4646 <52617166+nitin4646@users.noreply.github.com>
2023-12-23 04:12:07 +0530

    DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to Nitin Mhalim 23122023-0413";
 	}
 
 	public static void main(String[] args) {
